94bf4bb61af744750288402787f744a913c875a7
Tanish Khandelwal <76485783+tknishh@users.noreply.github.com>
2023-11-03 14:45:32 +0530

    Update generation.py

--- streamlit_apps/generation/generation.py
+++ streamlit_apps/generation/generation.py
@@ -1,51 +1,94 @@
-import os
-import time
 from dotenv import load_dotenv
-import pinecone
+import os
+import streamlit as st
 from PyPDF2 import PdfReader
+import textract
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
 load_dotenv()
+from PIL import Image
 
-data_dir = "data/"
-index_name = "legalease-nda"
+img = Image.open(r"streamlit_apps\understanding\logo.png")
+st.set_page_config(page_title="LegalEase: Generate with Ease", page_icon=img)
+st.header("LegalEase: Generate with Ease📄")
+file = st.file_uploader("Upload your file")
 
-# Connect to Pinecone
-pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment=os.getenv('PINECONE_ENV'))
-embeddings = OpenAIEmbeddings()
-index = pinecone.Index(index_name)
+if file is not None:
+    content = file.read()  # Read the file content once
 
-# Wait a moment for the index to be fully initialized
-time.sleep(1)
+    if file.type == 'application/pdf':
+        pdf_reader = PdfReader(file)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    elif file.type == 'text/plain':
+        text = content.decode('utf-8')
+    elif file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+        text = textract.process(content)
 
-# List PDF files in the data directory
-pdf_files = [file for file in os.listdir(data_dir) if file.endswith(".pdf")]
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )  
 
-# Extract text from multiple PDF files, embed it, and upload the vectors to Pinecone
-for pdf_file in pdf_files:
-    pdf_path = os.path.join(data_dir, pdf_file)
+    chunks = text_splitter.split_text(text)
 
-    # Load the text from the PDF file
-    pdf_reader = PdfReader(pdf_path)
-    text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text()
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-    # Encode the text into vectors using OpenAIEmbeddings
-    vectors = embeddings.embed_query(text)  # Embed the text into vectors
+    query = st.text_input("Ask you question")
+    if query:
+        docs = knowledge_base.similarity_search(query)
 
-    # Add the encoded vectors to the Pinecone index
-    pinecone.Index(index_name).upsert(ids=[pdf_file], vectors=vectors)
-
-
-# Query the index
-query = "This Agreement is made and entered into as of the Effective Date by and between"
-vectors = embeddings.embed_query(query)
-results = index.query(queries=vectors, top_k=5)
-
-# Print the results
-print("Query:", query)
-for result in results[0].results:
-	print(result.id, result.score)
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        response = chain.run(input_documents=docs, question=query)
+           
+        st.success(response)
 
 
+# import os
+# import time
+# from dotenv import load_dotenv
+# import pinecone
+# from PyPDF2 import PdfReader
+# from langchain.embeddings.openai import OpenAIEmbeddings
+
+# load_dotenv()
+
+# data_dir = "data/"
+# index_name = "legalease-nda"
+
+# # Connect to Pinecone
+# pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment=os.getenv('PINECONE_ENV'))
+# embeddings = OpenAIEmbeddings()
+# index = pinecone.Index(index_name)
+
+# # Wait a moment for the index to be fully initialized
+# time.sleep(1)
+
+# # List PDF files in the data directory
+# pdf_files = [file for file in os.listdir(data_dir) if file.endswith(".pdf")]
+
+# # Extract text from multiple PDF files, embed it, and upload the vectors to Pinecone
+# for pdf_file in pdf_files:
+#     pdf_path = os.path.join(data_dir, pdf_file)
+
+#     # Load the text from the PDF file
+#     pdf_reader = PdfReader(pdf_path)
+#     text = ""
+#     for page in pdf_reader.pages:
+#         text += page.extract_text()
+
+#     # Encode the text into vectors using OpenAIEmbeddings
+#     vectors = embeddings.embed_query(text)  # Embed the text into vectors
+
+#     # Add the encoded vectors to the Pinecone index
+#     pinecone.Index(index_name).upsert(ids=[pdf_file], vectors=vectors)
